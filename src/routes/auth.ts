0caa83661562a232bbaa63b31b2dc2fc00285ffd
Rayzon3 <rahul.03111999@gmail.com>
2021-06-19 21:11:21 +0530

    pushed

---
+++ src/routes/auth.ts
@@ -0,0 +1,25 @@
+import { Request, Response, Router } from "express";
+import { User } from "../entities/User";
+
+const register = async (req: Request, res: Response) => {
+    const { email, username, password } = req.body
+
+    try {
+        //validate data
+
+        //create user
+        const user = new User({ email, username, password })
+        await user.save()
+
+        //return user
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+}
+
+const router = Router()
+router.post("/register", register)
+
+export default router
