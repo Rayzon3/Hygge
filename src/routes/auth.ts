55215e0a36710af70cd38405d55b727e770888b5
Rayzon3 <rahul.03111999@gmail.com>
2021-06-20 16:09:38 +0530

    cookies, login, logout

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,11 @@
 import { Request, Response, Router } from "express";
-import { validate } from "class-validator"
+import { validate, isEmpty } from "class-validator"
 import { User } from "../entities/User";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import cookie from "cookie"
+import { error } from "console";
+import auth from "../middleware/auth"
 
 
 const register = async (req: Request, res: Response) => {
@@ -33,7 +38,60 @@ const register = async (req: Request, res: Response) => {
     }
 }
 
+
+const login = async (req: Request, res: Response) => {
+    const { username, password } = req.body
+
+    try {
+        let errors: any = {}
+
+        if(isEmpty(username)) errors.username = "Username field must not be empty !!"
+        if(isEmpty(password)) errors.password = "Password field must not be empty !!"
+        if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+        const user = await User.findOne({ username })
+        if(!user) return res.status(404).json({ error: "Worng username and password combination !!" })
+
+        const passwordMatches = await bcrypt.compare(password, user.password) 
+        if(!passwordMatches) return res.status(401).json({ error: "Worng username and password combination !!" })
+        
+        //gen JWT
+        const token = jwt.sign({ username }, process.env.JWT_SECRET) // idealing should be base62
+        //store JWT in cookie
+        res.set("Set-Cookie", cookie.serialize("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 3600,
+            path: "/" //where cookie is valid, the whole site
+        }))
+        return res.json(user)
+
+    } catch (error) {
+        
+    }
+}
+
+const logout = (_, res: Response) => {
+    res.set("Set-Cookie", cookie.serialize("token", "", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        expires: new Date(0),// expire immediately
+        path: "/" //where cookie is valid, the whole site
+    }))
+
+    return res.status(200).json({ success: true })
+}
+
+const me =  (_, res: Response) => {
+   return res.json(res.locals.user)
+}
+
 const router = Router()
 router.post("/register", register)
+router.post("/login", login)
+router.get("/me", auth, me)
+router.get("/logout", auth, logout)
 
 export default router
